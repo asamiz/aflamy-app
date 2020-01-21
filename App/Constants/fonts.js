caa5485be68e0c54d822fc374b8e4eca53e8151e
Ahmed Sami <asami@rasan.co>
2020-01-21 14:30:23 +0200

    add app icons for ios

--- App/Constants/fonts.js
+++ App/Constants/fonts.js
@@ -1,47 +1,50 @@
+import { Platform } from 'react-native'
+
+
 export const Fonts = {
     h1: {
         fontSize: 30,
         lineHeight: 48,
-        fontFamily: 'Overpass-Light'
+        fontFamily: Platform.OS == 'android' ? 'Overpass-Light' : 'Overpass_Light'
     },
     h2: {
         fontSize: 26,
         lineHeight: 30,
-        fontFamily: 'Overpass-Light'
+        fontFamily: Platform.OS == 'android' ? 'Overpass-Light' : 'Overpass_Light'
     },
     s1: {
         fontSize: 16,
         lineHeight: 30,
-        fontFamily: 'Overpass-Bold'
+        fontFamily: Platform.OS == 'android' ? 'Overpass-Bold' : 'Overpass_Bold'
     },
     s2: {
         fontSize: 16,
         lineHeight: 24,
-        fontFamily: 'Overpass-Light'
+        fontFamily: Platform.OS == 'android' ? 'Overpass-Light' : 'Overpass_Light'
     },
     s4: {
         fontSize: 11,
         lineHeight: 16,
-        fontFamily: 'Overpass-Bold'
+        fontFamily: Platform.OS == 'android' ? 'Overpass-Bold' : 'Overpass_Bold'
     },
     s3i: {
         fontSize: 14,
         lineHeight: 16,
-        fontFamily: 'Overpass-Italic'
+        fontFamily: Platform.OS == 'android' ? 'Overpass-Italic' : 'Overpass_Italic'
     },
     s4i: {
         fontSize: 11,
         lineHeight: 16,
-        fontFamily: 'Overpass-Italic'
+        fontFamily: Platform.OS == 'android' ? 'Overpass-Italic' : 'Overpass_Italic'
     },
     button: {
         fontSize: 12,
         lineHeight: 20,
-        fontFamily: 'Overpass-Bold'
+        fontFamily: Platform.OS == 'android' ? 'Overpass-Bold' : 'Overpass_Bold'
     },
     Input: {
         fontSize: 16,
         lineHeight: 25,
-        fontFamily: 'Overpass-Bold'
+        fontFamily: Platform.OS == 'android' ? 'Overpass-Bold' : 'Overpass_Bold'
     }
 }
